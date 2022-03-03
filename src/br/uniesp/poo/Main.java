405bfc6ba5a642be35bbd0870d8a3d0570b74398
Fujioka <rodrigofujioka@gmail.com>
2022-03-03 18:38:26 -0300

    Aulas de Linguagem de programação I Uniesp

---
+++ src/br/uniesp/poo/Main.java
@@ -0,0 +1,10 @@
+package br.uniesp.poo;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+	  System.out.print("Hello World");
+    }
+
+}
---
+++ src/br/uniesp/poo/aula02/UsaCachorro.java
@@ -0,0 +1,13 @@
+package br.uniesp.poo.aula02;
+
+public class UsaCachorro {
+
+    public static void main(String[] args) {
+
+        Cachorro viraLata = new Cachorro();
+
+        viraLata.anda();
+        viraLata.late();
+
+    }
+}
